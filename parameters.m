25aa4bc5e0e2e90c209ab003fc94909a2cf158e4
Nimit <nimitsohoni@gmail.com>
2015-10-30 18:17:16 -0400

    updating parameters

--- parameters.m
+++ parameters.m
@@ -1,9 +1,11 @@
 ticks_per_minute = 5;
-xdim = 20;
-ydim = 150;
-density = ones(xdim,ydim);
+
+sight_radius = 1;
+xdim = ceil(20 / sight_radius);
+ydim = ceil(150 / sight_radius);
+visit_interval = ones(xdim,ydim) * 15 * ticks_per_minute;
 A = repmat((1:xdim)',1,ydim);
 B = repmat(1:ydim,xdim,1);
-center_x = floor(mean(mean(density.*B))); % compute center of mass
-center_y = floor(mean(mean(density.*A)));
+center_x = floor(mean(mean(visit_interval.*B))); % compute center of mass
+center_y = floor(mean(mean(visit_interval.*A)));
 p = 0.3;
