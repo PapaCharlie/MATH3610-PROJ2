5a107cde60d30e3a1174ec5c9934de1299a28b8c
Paul Chesnais <paul.chesnais@gmail.com>
2015-11-01 18:37:07 -0500

    Added my script

---
+++ MAV.m
@@ -0,0 +1,51 @@
+classdef MAV < handle
+  properties
+    fuel_range
+    location
+    quadrant
+    position
+  end
+
+  methods
+    function self = MAV(quadrant)
+      global fuel_range;
+      self.fuel_range = fuel_range;
+      self.quadrant = quadrant;
+      self.position = [ quadrant(1) quadrant(2) ];
+      quadrant
+    end
+
+    function step(self)
+      global speed;
+      global tick;
+      global last_visited;
+      self.fuel_range = self.fuel_range - speed;
+      for n = 1:speed
+        last_visited(self.position(1) + 1, self.position(2) + 1) = tick;
+        if isequal(self.position, self.quadrant(1:2)) % at starting point
+          self.position = self.position + [ 0 1 ];
+
+        elseif self.position(2) == self.quadrant(2)
+          self.position = self.position + [ -1 0 ];
+
+        elseif self.position(1) == self.quadrant(3)
+          self.position = self.position + [ 0 -1 ];
+
+        elseif mod(self.position(1) - self.quadrant(1), 2) == 0
+          if self.position(2) == self.quadrant(4)
+            self.position = self.position + [ 1 0 ];
+          else
+            self.position = self.position + [ 0 1 ];
+          end
+
+        elseif mod(self.position(1) - self.quadrant(1), 2) == 1
+          if self.position(2) == self.quadrant(2) + 1
+            self.position = self.position + [ 1 0 ];
+          else
+            self.position = self.position + [ 0 -1 ];
+          end
+        end
+      end
+    end
+  end
+end
--- parameters.m
+++ parameters.m
@@ -1,8 +1,24 @@
+global ticks_per_minute;
+global speed;
+global fuel_range;
+global refuel_time;
+global sight_radius;
+global xdim;
+global ydim;
+global visit_interval;
+global A;
+global B;
+global center_x;
+global center_y;
+global p;
+
 ticks_per_minute = 5;
 
-speed = 5;
+speed = 10/ticks_per_minute; % in blocks per tick
 
 fuel_range = ticks_per_minute * 60 * 5 * speed; % in blocks
+
+refuel_time = ticks_per_minute * 60 * 5; % hours
 
 sight_radius = 1;
 xdim = ceil(20 / sight_radius);
