c1b750f1ce2150611c5ca223b236f94e4166b5e7
Paul Chesnais <paul.chesnais@gmail.com>
2015-11-03 18:25:38 -0500

    Changed the parameters file to be more realistic

--- parameters.m
+++ parameters.m
@@ -4,7 +4,7 @@ global fuel_range;
 global refuel_time;
 global xdim;
 global ydim;
-% global visit_interval;
+global visit_interval;
 global A;
 global B;
 global center_x;
@@ -20,9 +20,19 @@ fuel_range = ticks_per_minute * 60 * 5 * speed; % in blocks
 
 refuel_time = ticks_per_minute * 60 * 5; % hours
 
-xdim = 20;
+xdim = 30;
 ydim = 250;
-visit_interval = ones(xdim,ydim) * 15 * ticks_per_minute;
+
+visit_interval = ones(xdim,ydim);
+visit_interval(:,:) = 15;
+% Set Central Park
+visit_interval(10:19, 59:110) = 20;
+% Set Gotham University
+visit_interval(1:6, 110:120) = 5;
+% Set Financial District
+visit_interval(:, 1:20) = 5;
+visit_interval = visit_interval * ticks_per_minute;
+
 A = repmat((1:xdim)',1,ydim);
 B = repmat(1:ydim,xdim,1);
 center_x = floor(mean(mean(visit_interval.*B))); % compute center of mass
