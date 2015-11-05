fc8859a89983e39f999415fa91918c21d15a72d9
Paul Chesnais <paul.chesnais@gmail.com>
2015-11-04 20:03:23 -0500

    Added more figures to the manuscript

--- models/random_movement.m
+++ models/random_movement.m
@@ -43,9 +43,10 @@ for num_mavs = 1:max_mavs
 end
 save 'mean_per_drone_part1.mat' mean_per_drone
 
+figure
 plot(mean_per_drone, 'LineWidth', 2)
-% title('Percent of Poorly Monitored Zones vs Number of MAVs (uniform urgency)', 'Units', 'normalized', 'Position', [1 1])
-title 'Percent of Poorly Monitored Zones vs Number of MAVs (uniform urgency)'
+ylim([0 1])
+title({'Percent of Poorly Monitored Zones vs'; 'Number of MAVs (uniform urgency)'})
 xlabel 'Number of MAVs'
 ylabel 'Poorly Monitored Zones (%)'
 saveas(gca, 'random_walk_part1.pdf')
@@ -89,8 +90,10 @@ for num_mavs = 1:max_mavs
 end
 save 'mean_per_drone_part3.mat' mean_per_drone
 
+figure
 plot(mean_per_drone, 'LineWidth', 2)
-title 'Percent of Poorly Monitored Zones vs Number of MAVs (non-uniform urgency)'
+ylim([0 1])
+title({'Percent of Poorly Monitored Zones vs'; 'Number of MAVs (non-uniform urgency)'})
 xlabel 'Number of MAVs'
 ylabel 'Poorly Monitored Zones (%)'
 saveas(gca, 'random_walk_part3.pdf')
